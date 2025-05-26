593af7e15d97dc7898e56eaa870a68dcb6261bdb
hdhillon23 <harvird106@deltalearns.ca>
2025-05-26 09:20:24 -0700

    Used a url link to get info for getting meal data from the API. Used a function.

--- API_Project.py
+++ API_Project.py
@@ -1,6 +1,13 @@
 # Imports
+import requests # For making API calls
+import tkinter as tk # For GUI diisplay
+import random # For selecting a random joke
 
 # Get meal data from API
+def get_meal(keyword):
+    url = f"https://www.themealdb.com/api/json/v1/1/search.php?s={keyword}"
+    respose = requests.get(url)
+    data = response.json()
 
 # Check if a meal is found
 
