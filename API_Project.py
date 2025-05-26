7a8ce7b488aab012a186c178f8d3a2665f7c915c
hdhillon23 <harvird106@deltalearns.ca>
2025-05-26 09:03:50 -0700

    Added pseudocode

---
+++ API_Project.py
@@ -0,0 +1,23 @@
+# Imports
+
+# Get meal data from API
+
+# Check if a meal is found
+
+# Get meal details with default values if missin
+
+# Find two ingredients
+
+# Fetch jokes from API
+
+# Filter jokes realted to food
+
+# Select a joke
+
+# Get meal details
+
+# Get a food related joke
+
+# Display meal info and joke
+
+# Set up GUI window
